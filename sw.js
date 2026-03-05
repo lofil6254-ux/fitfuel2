224b3c00eb036b3524ebdc0e1df34fb14173f233
lofil6254-ux <lofil6254@gmail.com>
2026-03-05 13:42:47 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'fitfuel-v19';
+const CACHE_VERSION = 'fitfuel-v20';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.jpg'];
 
 self.addEventListener('install', e => {
